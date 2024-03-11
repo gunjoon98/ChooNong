ac4676f8423560533a739ebb16cd70d18a6aa819
정건준 <dlans321@naver.com>
2024-03-11 15:17:50 +0900

    Chore: SpringBoot 프로젝트 생성

---
+++ backend/choonong/src/main/java/com/ssafy/choonong/ChoonongApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.choonong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoonongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoonongApplication.class, args);
+	}
+
+}
