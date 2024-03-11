e1e8a03e4a39123d7f9475b91275f6b24b1e828d
geunhyeongKim <ppoyam_2@naver.com>
2024-03-11 09:16:09 +0900

    Depend:의존성주입_240311

---
+++ backend/choonong/src/test/java/com/example/choonong/ChoonongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.choonong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoonongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
