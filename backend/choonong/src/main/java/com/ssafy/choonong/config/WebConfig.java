7b9fbcfe2e143d95294acb779bcde9f7613b579f
정건준 <dlans321@naver.com>
2024-03-27 15:23:36 +0900

    Feat: 추가 CORS

---
+++ backend/choonong/src/main/java/com/ssafy/choonong/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.ssafy.choonong.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("GET", "POST", "HEAD");
+    }
+}
