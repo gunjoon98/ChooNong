ea1483eebc9b84fd5b6de5915547d4d310f9d83a
euichansong <zxcasdqwer38@naver.com>
2024-04-02 15:35:58 +0900

    Fix: 수정 CORS

--- fast_api/main.py
+++ fast_api/main.py
@@ -14,7 +14,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ['http://localhost:8080', '*']
+origins = ['http://localhost:5173', '*']
 
 app.add_middleware(
     CORSMiddleware,
