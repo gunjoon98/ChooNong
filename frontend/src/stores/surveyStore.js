59660b683450c672cce4edf736be6bdb0a75f5ed
김소연 <summerfallwin@naver.com>
2024-04-02 16:38:13 +0900

    Fix: 수정 설문 api 연동 시도 중

--- frontend/src/stores/surveyStore.js
+++ frontend/src/stores/surveyStore.js
@@ -1,27 +1,31 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import axios from "axios";
 
 export const useSurveyStore = defineStore('survey', () => {
 
+  const structuredResponsesToJson = (structuredResponses) => {
+    return JSON.stringify(structuredResponses);
+  };
   
   const resultList = ref([]);
-  const getSurveyResult = async function (structuredResponses) {
-    console.log("structuredResponses" + structuredResponses);
-    await axios({
+
+  const getSurveyResult = function (structuredResponses) {
+    return axios({
       method: "POST",
       url: "https://www.choonong.store/fapi/recommendation",
       data: structuredResponsesToJson(structuredResponses),
-      contentType: "application/json",
+      headers: {
+        "Content-Type": "application/json",
+      },
     }).then((response) => {
       resultList.value = response.data;
-      console.log("응답 결과" + resultList.value);
+      console.log("응답 결과", resultList.value);
+      return response.data;
+    }).catch((error) => {
+      console.error("요청 실패:", error);
+      throw error; // 이 부분은 필요에 따라 변경할 수 있습니다.
     });
-  };
-
-  const structuredResponsesToJson = (structuredResponses) => {
-    console.log(JSON.stringify(structuredResponses));
-    return JSON.stringify(structuredResponses);
   };
 
   return {
